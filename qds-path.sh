0740f5afdecd2c9e86e1c782c0415dc63fadca63
Vladimir Kozak <vkozak85@gmail.com>
2026-07-24 12:55:46 +0200

    deployQd gradle task

--- qds-path.sh
+++ qds-path.sh
@@ -1,12 +1,17 @@
-
+if [[ "$OS" == "Windows_NT" ]]; then
+    PATH_SEP=";"
+else
+    PATH_SEP=":"
+fi
 
 QDS_LIBS_DIR="libs/3.347/"
 QDS_SUFFIX="-3.347.jar"
 QDS_PATH="."
 
 for lib in "qds-tools" "qds" "dxlib" "qds-file" "mars" "qds-monitoring" "dxfeed-api"; do
-  QDS_PATH="$QDS_PATH;$QDS_LIBS_DIR$lib$QDS_SUFFIX"
+  lib_path="${QDS_LIBS_DIR}${lib}${QDS_SUFFIX}"
+  QDS_PATH="${QDS_PATH}${PATH_SEP}${lib_path}"
 done
 
 # add custom tools
-QDS_PATH="$QDS_PATH;libs/config-1.4.3.jar"
+QDS_PATH="${QDS_PATH}${PATH_SEP}libs/config-1.4.3.jar"
